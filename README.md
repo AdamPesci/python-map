018703584bc9c6f59c5f57a508c24f1bc7e5f700
Adam Pesci <adamcamerpesci@gmail.com>
2023-05-13 14:02:17 +0800

    refactored

--- README.md
+++ README.md
@@ -11,7 +11,18 @@ This is an interactive map exercise that uses the Folium Python package. The goa
 
 ### Data
 
-- Natural Earth data: https://www.naturalearthdata.com/downloads/ The code assumes medium scale 1:50m is used. You'll need the cultural and physical data. Place them in the `assets/natural_earth/50/` directory.
+- Natural Earth data: https://www.naturalearthdata.com/downloads/
+
+You'll need the 1:10m scale data.  
+
+Specifically:
+            - ocean: ne_10m_ocean.shp
+            - gridlines: ne_10m_geographic_lines.shp
+            - coasline: ne_10m_coastline.shp
+            - minor_islands: 10m/ne_10m_minor_islands.shp
+
+However downloading all the data at once doesn't take long and isn't very big so i'd just download 10m and 50m and place them in the `assets/natural_earth/10m/`  and `assets/natural_earth/50m/` directories respectively.  
+
 
 ## Setup
 
@@ -21,5 +32,28 @@ This is an interactive map exercise that uses the Folium Python package. The goa
     - For Windows: `.venv\Scripts\activate`
     - For Linux: `source .venv/bin/activate`
 4. Install Node.js modules using the following command: `npm install`
-5. Copy `leaflet.awesome.rotate.min.css` into `node_modules/leaflet/dist/`.
 6. Run the code as required.
+
+# Config
+If you want to use the maps without an internet connection you can update the .config files settings.   
+  
+The deafults are:
+```shellscript
+NE_SCALE=10
+```
+```shellscript
+OFFLINE_MAP=False
+```
+```shellscript
+LEAFLET_JS=node_modules/leaflet/dist/leaflet.js
+AWESOME_MARKERS_JS=node_modules/leaflet.awesome-markers/dist/leaflet.awesome-markers.js
+JQUERY_JS=node_modules/jquery/dist/jquery.min.js
+BOOTSTRAP_JS=node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+```
+```shellscript
+LEAFLET_CSS=node_modules/leaflet/dist/leaflet.css
+AWESOME_MARKERS_CSS=node_modules/leaflet.awesome-markers/dist/leaflet.awesome-markers.css
+AWESOME_ROTATE_CSS=assets/js/leaflet.awesome.rotate.min.css
+BOOTSTRAP_CSS=node_modules/bootstrap/dist/css/bootstrap.min.css
+FONTAWESOME_FREE_CSS=node_modules/fontawesome-free/css/all.min.css
+```
--- map.py
+++ map.py
@@ -1,171 +1,105 @@
 import folium
 import geopandas as gpd
-import random
-import re
-import glob
 from uuid import uuid4
+from dotenv import dotenv_values
+CONFIG = dotenv_values('.config')
+SCALE = CONFIG.get('NE_SCALE', '10')
 
-'''
-Replace cdn links with using static paths
-'''
-def static_replace_with_local(html_string):
-    # Replace external references with local ones (allows user to manually change if replace with local doesnt work)
-    replacements = {
-        "https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/leaflet.js": "assets/leaflet.js",
-        "https://code.jquery.com/jquery-1.12.4.min.js": "assets/jquery.min.js",
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js": "assets/bootstrap.bundle.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.js": "assets/leaflet.awesome-markers.js",
-        "https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/leaflet.css": "assets/leaflet.css",
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css": "assets/bootstrap.min.css",
-        "https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css": "assets/bootstrap-3.0.0.min.css",
-        "https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/all.min.css": "assets/fontawesome-free-6.2.0.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/Leaflet.awesome-markers/2.0.2/leaflet.awesome-markers.css": "assets/leaflet.awesome-markers.css",
-        "https://cdn.jsdelivr.net/gh/python-visualization/folium/folium/templates/leaflet.awesome.rotate.min.css": "assets/leaflet.awesome.rotate.min.css",
-    }
-
-    for external, local in replacements.items():
-        html_string = html_string.replace(external, local)
-
-    return html_string
-
-'''
-Replace cdn links using regex and node modules
-'''
-def replace_with_local_wrapper(html_string, static):
-    # Replace external references with local ones
-    
-    if static:
-        return static_replace_with_local(html_string)
-    
-    html_string = re.sub(
-        r"https://[^/]+/([^/]+/)*([^/]+?)(\.js|\.css)", replace_with_local, html_string
-    )
-    return html_string
-
-def replace_with_local(match):
-    patterns_to_local = {
-        r"leaflet(?!\.awesome-markers)": "node_modules/leaflet",
-        r"jquery": "node_modules/jquery",
-        r"bootstrap": "node_modules/bootstrap",
-        r"Leaflet\.awesome-markers": "node_modules/leaflet.awesome-markers",
-        r"@fortawesome/fontawesome-free": "node_modules/fontawesome-free",
-    }
-
-    for pattern, local_path in patterns_to_local.items():
-        if re.search(pattern, match.group(0)):
-            local_path_versions = glob.glob(f"{local_path}@*/")
-            if not local_path_versions:
-                local_path_versions = glob.glob(f"{local_path}*/")
-
-            local_path_version = local_path_versions[0]
-
-            folder = "dist"
-            subfolder = ""
-            if pattern == r"jquery":
-                folder = "dist"
-                file_name = "jquery.min.js"
-            elif pattern == r"bootstrap":
-                folder = "dist"
-                subfolder = "js" if ".js" in match.group(0) else "css"
-                file_name = match.group(0).rsplit("/", 1)[-1]
-                if subfolder == "js" and file_name.endswith(".css"):
-                    subfolder = "css"
-            elif pattern == r"Leaflet\.awesome-markers":
-                folder = "dist"
-                file_name = match.group(0).rsplit("/", 1)[-1]
-            elif pattern == r"@fortawesome/fontawesome-free":
-                folder = "css"
-                file_name = "all.min.css"
-            elif pattern == r"leaflet(?!\.awesome-markers)":
-                folder = "dist"
-                file_name = match.group(0).rsplit("/", 1)[-1]
-
-            return f"{local_path_version}{folder}/{subfolder}/{file_name}" if subfolder else f"{local_path_version}{folder}/{file_name}"
-
-    return match.group(0)
 
 '''
 Creates a folium map with geopandas data
 '''
 def create_map():
     # Read shapefiles for map layers from local files
-    countries = gpd.read_file("assets/natural_earth/50m/ne_50m_admin_0_countries.shp")
-    ocean = gpd.read_file("assets/natural_earth/50m/ne_50m_ocean.shp")
-    gridlines = gpd.read_file("assets/natural_earth/50m/ne_50m_geographic_lines.shp")
-    # rivers = gpd.read_file("assets/natural_earth/50m/ne_50m_rivers_lake_centerlines.shp")
-    # lakes = gpd.read_file("assets/natural_earth/50m/ne_50m_lakes.shp")
+    ocean = gpd.read_file(f'assets/natural_earth/{SCALE}m/ne_{SCALE}m_ocean.shp')
+    gridlines = gpd.read_file(f'assets/natural_earth/{SCALE}m/ne_{SCALE}m_geographic_lines.shp')
+    coastline = gpd.read_file(f'assets/natural_earth/{SCALE}m/ne_{SCALE}m_coastline.shp')
+    # minor islands only come as 10m
+    minor_islands = gpd.read_file(f'assets/natural_earth/10m/ne_10m_minor_islands.shp')
 
-    # Define color palette for countries
-    color_palette = [
-        "#e6e6e6", "#f5b897", "#c5e5a4", "#b3d0d8", "#f0e1a1", "#f3c29d", "#c5d9a5",
-        "#d4c6a2", "#b3d0d8", "#d4c6a2", "#c5d9a5", "#d4c6a2", "#f0e1a1"
+    # Create folium GeoJson objects for each layer with appropriate styles
+    layers = [
+        folium.GeoJson(
+            data=ocean.to_json(),
+            style_function=lambda x: {'color': '#a9eafc', 'weight': 0.5},
+            name='Ocean', show=True
+        ),
+
+        folium.GeoJson(
+            data=gridlines.to_json(),
+            style_function=lambda x: {
+                'color': '#000000', 'weight': 0.5, 'dashArray': '5, 5'},
+            name='Gridlines', show=False
+        ),
+        folium.GeoJson(
+            data=coastline.to_json(),
+            style_function=lambda x: {'color': '#000000', 'weight': 0.5},
+            name='Coastline', show=True
+        ),
+
+        folium.GeoJson(
+            data=minor_islands.to_json(),
+            style_function=lambda x: {'color': '#000000', 'weight': 0.5},
+            name='Minor Islands', show=True
+        ),
     ]
 
-    # Assign random colors to each country
-    countries['color'] = [random.choice(color_palette) for _ in range(len(countries))]
-
-
-     # Create folium GeoJson objects for each layer with appropriate styles
-    style = {"color": "#000000", "weight": 1, "fillOpacity": 0.7}
-    countries_json = folium.GeoJson(
-        data=countries.to_json(),
-        style_function=lambda x: dict(style, fillColor=x['properties']['color']),
-        name='Countries', show=True
-    )
-
-    ocean_json = folium.GeoJson(
-        data=ocean.to_json(),
-        style_function=lambda x: {"color": "#a9eafc", "weight": 0.5},
-        name='Ocean', show=True
-    )
-
-    lines_json = folium.GeoJson(
-        data=gridlines.to_json(),
-        style_function=lambda x: {"color": "#000000", "weight": 0.5, "dashArray": "5, 5"},
-        name='Gridlines', show=False
-    )
-
-    # rivers_json = folium.GeoJson(
-    #     data=rivers.to_json(),
-    #     style_function=lambda x: {"color": "#a9eafc", "weight": 0.5},
-    #     name='Rivers', show=False
-    # )
-
-    # lakes_json = folium.GeoJson(
-    #     data=lakes.to_json(),
-    #     style_function=lambda x: {"color": "#a9eafc", "weight": 0.5},
-    #     name='Lakes', show=False
-    # )
-
-    # Create a folium map and add layers to it
-    sw = countries.total_bounds[[1, 0]]
-    ne = countries.total_bounds[[3, 2]]
+    # set bounds of map around ocean so user is forced to stay within the map
+    sw = ocean.total_bounds[[1, 0]]
+    ne = ocean.total_bounds[[3, 2]]
     m = folium.Map(location=[0, 0], tiles=None, zoom_start=1.5, maxBounds=[sw.tolist(), ne.tolist()])
     m.options.update(minZoom=2)
-    
-    
+
     # Add the layers to the map
-    for layer in [countries_json, ocean_json, lines_json]:
+    for layer in layers:
         layer.add_to(m)
-    
+
     # Add a layer control to the map
     folium.LayerControl().add_to(m)
+    
+    # Override the deafult css and js to local paths if offline map set in config
+    if CONFIG.get('OFFLINE_MAP') == 'True':
+        override_default_js_css(m)
 
     return m
 
+
 '''
-Saves the folium map as an HTML file
-'''   
-def save_map_as_html(m, save_as=None, static=False):
-    out_path = save_as if save_as else f'map_{uuid4()}.html'
-    html_string = replace_with_local_wrapper(m.get_root().render(), static)
-    with open(out_path, "w") as f:
-        f.write(html_string)
+Overrides the default js/css paths
+'''
+def override_default_js_css(m):
+    for i, (name, _) in enumerate(m.default_js):
+            if name == 'leaflet':
+                link = CONFIG['LEAFLET_JS']
+            elif name == 'jquery':
+                link = CONFIG['JQUERY_JS']
+            elif name == 'bootstrap':
+                link = CONFIG['BOOTSTRAP_JS']
+            elif name == 'awesome_markers':
+                link = CONFIG['AWESOME_MARKERS_JS']
+            m.default_js[i] = (name, link)
+
+    # bootstrap3 glyphicons not required with bootstrap5
+    gli_index = 0
+    for i, (name, _) in enumerate(m.default_css):
+        if name == 'leaflet_css':
+            link = CONFIG['LEAFLET_CSS']
+        elif name == 'bootstrap_css':
+            link = CONFIG['BOOTSTRAP_CSS']
+        elif name == 'glyphicons_css':
+            gli_index = i
+        elif name == 'awesome_markers_font_css':
+            link = CONFIG['FONTAWESOME_FREE_CSS']
+        elif name == 'awesome_markers_css':
+            link = CONFIG['AWESOME_MARKERS_CSS']
+        elif name == 'awesome_rotate_css':
+            link = CONFIG['AWESOME_ROTATE_CSS']
+        m.default_css[i] = (name, link)
+    m.default_css.pop(gli_index)
     
 def main():
     m = create_map()
-    save_map_as_html(m)
-    
+    m.save(f'map_{uuid4()}.html')
+        
 if __name__ == '__main__':
     main()
-
